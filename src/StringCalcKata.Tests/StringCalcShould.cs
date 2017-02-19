cc8736c8ec5be61555c28db1b88eb84af5edd5e2
Geoff Butler <geoffbutler@users.noreply.github.com>
2017-02-19 14:09:09 +1300

    Add code

---
+++ src/StringCalcKata.Tests/StringCalcShould.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace StringCalcKata.Tests
+{
+    [TestClass]
+    public class StringCalcShould
+    {
+        private StringCalc _target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _target = new StringCalc();
+        }
+
+        [TestMethod]
+        public void BeInstantiable()
+        {
+            _target.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void ReturnZeroForEmptyString()
+        {
+            _target.Add(string.Empty).Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ReturnASingleNumber()
+        {
+            _target.Add("1").Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            _target.Add("1,2").Should().Be(3);
+            _target.Add("2,3").Should().Be(5);
+        }
+
+        [TestMethod]
+        public void AddAnyNumberOfNumbers()
+        {
+            _target.Add("1,1,1").Should().Be(3);
+            _target.Add("1,2,3,4").Should().Be(10);
+            _target.Add(",1,2,3,4,5,").Should().Be(15);
+        }
+
+        [TestMethod]
+        public void HandleNewlineInsteadOfCommasForDelim()
+        {            
+            _target.Add("\n1\n2\n3\n4\n5\n").Should().Be(15);
+        }
+        
+        [TestMethod]
+        public void HandleSingleCharCustomDelim()
+        {
+            _target.Add("//;\n1;2").Should().Be(3);
+        }
+
+        [TestMethod]
+        public void ThrowWhenSuppliedNegativeNumbers()
+        {
+            new Action(() => _target.Add("-1")).ShouldThrow<Exception>().WithMessage("negatives not allowed: -1");
+            new Action(() => _target.Add("-1,-2")).ShouldThrow<Exception>().WithMessage("negatives not allowed: -1,-2");
+        }
+
+        [TestMethod]
+        public void IgnoreNumbersGreaterThan1000()
+        {
+            _target.Add("1000,1,1001,2,2003,3,3004").Should().Be(1006);
+        }
+
+        [TestMethod]
+        public void HandleMultiCharCustomDelim()
+        {
+            _target.Add("//[***]\n1***2***3").Should().Be(6);
+        }
+
+        [TestMethod]
+        public void HandleMultipleSingleCharCustomDelim()
+        {
+            _target.Add("//[*][%]\n1*2%3").Should().Be(6);
+        }
+
+        [TestMethod]
+        public void HandleMultipleMultiCharCustomDelim()
+        {
+            _target.Add("//[***][%%%]\n1***2%%%3").Should().Be(6);
+        }
+    }
+}
---
+++ src/StringCalcKata/StringCalc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalcKata
+{
+    public class StringCalc
+    {
+        public int Add(string input)
+        {
+            if (input == string.Empty) return 0;
+            
+            var nums = ParseInput(input);
+            
+            NegativeNumberCheck(nums);
+
+            return nums.Sum();
+        }
+        
+        private static List<int> ParseInput(string input)
+        {
+            string[] delims;
+            string numberInput;
+            GetDelimitersAndNumberInput(input, out delims, out numberInput);
+
+            var numberStrings = numberInput.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+
+            return numberStrings.Select(int.Parse)
+                .Where(num => num <= 1000) // exclude numbers greater than 1000
+                .ToList();
+        }
+
+        private static void GetDelimitersAndNumberInput(string input, out string[] delims, out string numberInput)
+        {
+            if (!input.StartsWith("//"))
+            {
+                // no custom delims
+                delims = new[] { ",", "\n" }; // default 
+                numberInput = input;
+                return;
+            }
+
+            if (!input.StartsWith("//["))
+            {
+                // single char custom delim                
+                delims = new[] { input.Substring(2, 1) };
+                numberInput = input.Substring(3);
+                return;
+            }
+
+            // multi char custom delim                        
+            var delimRegex = new Regex(@"\[{1}[^\]\[]*\]{1}");
+            var delimMatches = delimRegex.Matches(input);            
+            var multiCharDelims = new List<string>();
+            var delimLength = 2; // skip '//'
+            foreach (Match match in delimMatches)
+            {
+                delimLength += match.Length;
+                multiCharDelims.Add(match.Value.TrimStart('[').TrimEnd(']'));
+            }
+            var startOfNumberInput = delimLength + 1; // for the \n
+
+            delims = multiCharDelims.ToArray();
+            numberInput = input.Substring(startOfNumberInput);
+        }
+
+        private static void NegativeNumberCheck(IEnumerable<int> nums)
+        {
+            var negativeNums = nums.Where(num => num < 0).ToList();
+            if (negativeNums.Any())
+                throw new Exception("negatives not allowed: " + string.Join(",", negativeNums.Select(num => num.ToString())));
+        }
+    }
+}
